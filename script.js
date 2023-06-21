970be9e539c4c30afa99ce55e19bdc4efd2d37a9
genaro-cravero <nicocraveroo@gmail.com>
2023-06-21 13:42:22 -0300

    Ultima clase

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let pedidos = 0;
 let carrito = document.getElementById("carroNumero");
+const form = document.querySelector("#consultas");
+let consulta;
+let satisfaccion;
+
 
 carrito.innerHTML = 0;
 
@@ -26,3 +30,33 @@ function RestarComida()
     }
     carrito.innerHTML = pedidos;
 }
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    consulta = document.querySelector("#message").value;
+
+    satisfaccion = document.querySelector('input[name="satisfaccion"]:checked').value;
+
+    switch(satisfaccion)
+    {
+        case "0":
+            satisfaccion = "Muy mala";
+            break;
+        case "1":
+            satisfaccion = "Normal";
+            break;
+        case "2":
+            satisfaccion = "Muy buena";
+            break;
+    }
+
+    alert("Su consulta ha sido enviada exitosamente");
+
+    
+    console.log("La consulta fue: " + consulta + ". La satisfaccion del usuario fue " + satisfaccion);
+
+    form.reset();
+});
+
